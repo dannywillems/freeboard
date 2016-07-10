65f16ce71b15e682679e88c86c064c4deac5e47c
Danny Willems <contact@danny-willems.be>
2016-07-10 12:46:45 +0200

    Grunt: remove tabs and use spaces, add less directive but doesn't work. Create less dir.

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,28 +19,28 @@ module.exports = function(grunt) {
                         'lib/js/thirdparty/underscore.js',
                         'lib/js/thirdparty/jquery.gridster.js',
                         'lib/js/thirdparty/jquery.caret.js',
-						'lib/js/thirdparty/jquery.xdomainrequest.js',
+                        'lib/js/thirdparty/jquery.xdomainrequest.js',
                         'lib/js/thirdparty/codemirror.js',
                     ]
                 ],
                 dest : 'js/freeboard.thirdparty.js'
             },
-			fb : {
-				src : [
-					'lib/js/freeboard/DatasourceModel.js',
-					'lib/js/freeboard/DeveloperConsole.js',
-					'lib/js/freeboard/DialogBox.js',
-					'lib/js/freeboard/FreeboardModel.js',
-					'lib/js/freeboard/FreeboardUI.js',
-					'lib/js/freeboard/JSEditor.js',
-					'lib/js/freeboard/PaneModel.js',
-					'lib/js/freeboard/PluginEditor.js',
-					'lib/js/freeboard/ValueEditor.js',
-					'lib/js/freeboard/WidgetModel.js',
-					'lib/js/freeboard/freeboard.js',
-				],
-				dest : 'js/freeboard.js'
-			},
+            fb : {
+                src : [
+                    'lib/js/freeboard/DatasourceModel.js',
+                    'lib/js/freeboard/DeveloperConsole.js',
+                    'lib/js/freeboard/DialogBox.js',
+                    'lib/js/freeboard/FreeboardModel.js',
+                    'lib/js/freeboard/FreeboardUI.js',
+                    'lib/js/freeboard/JSEditor.js',
+                    'lib/js/freeboard/PaneModel.js',
+                    'lib/js/freeboard/PluginEditor.js',
+                    'lib/js/freeboard/ValueEditor.js',
+                    'lib/js/freeboard/WidgetModel.js',
+                    'lib/js/freeboard/freeboard.js',
+                ],
+                dest : 'js/freeboard.js'
+            },
             plugins : {
                 src : [
                     'plugins/freeboard/*.js'
@@ -54,6 +54,17 @@ module.exports = function(grunt) {
                 ],
                 dest : 'js/freeboard_plugins.js'
             }
+        },
+        less :
+        {
+          options:
+          {
+            paths: ["less"]
+          },
+          files:
+          {
+            "css/main.css": "less/main.less"
+          }
         },
         cssmin : {
             css:{
@@ -108,5 +119,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-string-replace');
-    grunt.registerTask('default', [ 'concat:css', 'cssmin:css', 'concat:fb', 'concat:thirdparty', 'concat:plugins', 'concat:fb_plugins', 'uglify:fb', 'uglify:plugins', 'uglify:fb_plugins', 'uglify:thirdparty', 'string-replace:css' ]);
+    grunt.registerTask('default', [ 'less', 'concat:css', 'cssmin:css', 'concat:fb', 'concat:thirdparty', 'concat:plugins', 'concat:fb_plugins', 'uglify:fb', 'uglify:plugins', 'uglify:fb_plugins', 'uglify:thirdparty', 'string-replace:css' ]);
 };
